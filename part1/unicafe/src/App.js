50a74b5bfcda876bc705f2613a3e195bb8861ad2
nicholasohjj <60575384+nicholasohjj@users.noreply.github.com>
2021-08-09 19:15:24 +0800

    added unicafe

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+const Header = ({course}) => {
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  )
+}
+
+const Part = ({title,value}) => {
+  return (
+    <div>
+      <p>{title} {value}</p>
+
+    </div>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      <Part title={parts[0].name} value={parts[0].exercises}/>
+      <Part title={parts[1].name} value={parts[1].exercises}/>
+      <Part title={parts[2].name} value={parts[2].exercises}/>
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  return (
+    <div>
+      <p>Number of exercises {parts[0].exercises+parts[1].exercises+parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
+
+
+    </div>
+  )
+}
+
+export default App
