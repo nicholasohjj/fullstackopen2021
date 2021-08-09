7c08e931d15a13e11a6e77ed8689e4215077c832
nicholasohjj <60575384+nicholasohjj@users.noreply.github.com>
2021-08-09 18:41:50 +0800

    completed courseinfo

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const Header = ({title}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{title}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+const Part = ({part,exercise}) => {
+  return (
+    <div>
+      <p>{part} {exercise}</p>
+
+    </div>
+  )
+}
+
+const Total = ({value}) => {
+  return (
+    <div>
+      <p>Number of exercises {value}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const total = exercises1+exercises2+exercises3
+
+  const Content = () => {
+    return (
+      <div>
+        <Part part={part1} exercise={exercises1}/>
+        <Part part={part2} exercise={exercises2}/>
+        <Part part={part3} exercise={exercises3}/>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Header title={course} />
+      <Content />
+      <Total value={total} />
+
+    </div>
+  )
+}
+
+export default App
