bd96c0863eb2890338c285945e6f56e6ae0674b4
nicholasohjj <60575384+nicholasohjj@users.noreply.github.com>
2021-08-13 18:21:57 +0800

    step 8

--- part2/information/src/App.js
+++ part2/information/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react' 
 
-function App() {
+const Course = ({courses}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header name={courses.name}/>
+      <Contents parts={courses.parts}/>
     </div>
-  );
+  )
 }
 
-export default App;
+const Index = (array) => {
+  {array}.map((value,index))=>
+  
+}
+
+const Header = ({name}) => <h1>{name}</h1>
+
+const Contents = ({parts}) => {
+  return (
+    <div>
+      <Part parts={parts}/>
+      <Total parts = {parts} />
+    </div>
+  )
+}
+
+const Part = ({parts}) => {
+  return (
+    <div>
+      {parts.map(part=>
+        <p key={part.id}>{part.name} {part.exercises}</p>
+      )}
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  const Exercises = parts.map(part=> part.exercises)
+
+  return (
+    <div>
+    <p>Total of {Exercises.reduce((total,sum) => total+sum)} exercises</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course courses={course} />
+
+
+}
+
+export default App
