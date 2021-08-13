6c8886481019549a8782714b5b6227b21fe49c9d
nicholasohjj <60575384+nicholasohjj@users.noreply.github.com>
2021-08-13 19:20:29 +0800

    completed 2.4

--- part2/information/src/App.js
+++ part2/information/src/App.js
@@ -9,11 +9,6 @@ const Course = ({courses}) => {
   )
 }
 
-const Index = (array) => {
-  {array}.map((value,index))=>
-  
-}
-
 const Header = ({name}) => <h1>{name}</h1>
 
 const Contents = ({parts}) => {
@@ -46,31 +41,56 @@ const Total = ({parts}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course courses={course} />
-
-
+  return (
+    <div>
+      {courses.map(course => <Course courses={course} />)}
+    </div>
+  )
 }
 
 export default App
